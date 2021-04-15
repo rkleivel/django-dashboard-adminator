86cf2dd2c086818f9b3c511484670d2c07d3ea70
Roald Kleiveland <roald.kleiveland@gmail.com>
2021-04-15 21:20:22 +0200

    More queries

--- queries.sql
+++ queries.sql
@@ -2,6 +2,8 @@ select * from app_rawfile where GPSfreq is NULL
 
 select count(*) from app_rawfile 
 
+select count(*) from app_gpsdata
+
 select * from app_rawfile where fileName ='GH023061'
 
 select DISTINCT timeZone from app_rawfile
@@ -11,3 +13,15 @@ select * from app_rawfile where GPSfreq is not NULL and GMTtime is NULL
 select * from app_rawfile where municipality = '' and county != ''
 
 select * from app_rawfile where country_int = '' and continent != ''
+
+PRAGMA foreign_keys = ON
+
+select count(*) from app_gpsdata 
+
+delete from app_rawfile where id < 2075
+
+update app_rawfile set trail = NULL where continent = 'America'
+
+delete from app_rawfile 
+
+delete from app_gpsdata 
